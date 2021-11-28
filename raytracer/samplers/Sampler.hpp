ce56ba8d35112f25057efef831f594a11b187b68
Adnan-Asif <60232439+Adnan-Asif@users.noreply.github.com>
2021-11-28 23:40:26 +0500

    Update Sampler.hpp

--- raytracer/samplers/Sampler.hpp
+++ raytracer/samplers/Sampler.hpp
@@ -41,6 +41,3 @@ public:
   virtual std::vector<Ray> get_rays(int px, int py) const = 0;
 };
 
-Sampler::Sampler() : camera_ptr(nullptr), viewplane_ptr(nullptr) {}
-
-Sampler::Sampler(Camera *c_ptr, ViewPlane *v_ptr): camera_ptr(c_ptr), viewplane_ptr(v_ptr) {}
