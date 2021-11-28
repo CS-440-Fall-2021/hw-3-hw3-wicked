d7ef05bbcf798aabf6ac5e2f279373d1d7e244d9
ar230500 <60130224+ar230500@users.noreply.github.com>
2021-11-28 23:42:05 +0500

    Update Ray.cpp

--- raytracer/utilities/Ray.cpp
+++ raytracer/utilities/Ray.cpp
@@ -1,20 +1,11 @@
 #include "Ray.hpp"
+#include <sstream>
 
-Ray()
-{
-    o(0, 0, 0);
-    d(0, 0, 0);
-    w = 1;
-}
+Ray::Ray(): o(0,0,0), d(0,0,0), w(1) {}
 
-Ray(const Point3D &origin, const Vector3D &dir)
-{
-    o(origin.x, origin.y, origin.z);
-    d(dir.x, dir.y, dir.z);
-    w = 1;
-}
+Ray::Ray(const Point3D& origin, const Vector3D& dir):o(origin), d(dir), w(1) {}
 
-std::string to_string()
+std::string Ray::to_string() const
 {
     std::stringstream s_w;
     s_w << w;
