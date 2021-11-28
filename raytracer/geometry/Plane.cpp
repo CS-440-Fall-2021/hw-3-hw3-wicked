1d6f86148d09fe806b29d5f679a4e8ccb6715468
Adnan-Asif <60232439+Adnan-Asif@users.noreply.github.com>
2021-11-28 23:38:51 +0500

    Update Plane.cpp

--- raytracer/geometry/Plane.cpp
+++ raytracer/geometry/Plane.cpp
@@ -1,51 +1,49 @@
 #include "Plane.hpp"
-#include "Geometry.hpp"
-#include "Vector3D.hpp"
-#include "Point3D.hpp"
+#include <sstream>
+#include "../utilities/Ray.hpp"
+#include "../utilities/Constants.hpp"
+#include "../utilities/ShadeInfo.hpp"
+#include "../utilities/BBox.hpp"
 
-Plane::Plane()
-{
-    a(0.0);
+Plane::Plane(): a(0), n(0,0,1) {}
+
+
+Plane::Plane(const Point3D &pt, const Vector3D &n) {
+    a = pt;
+    this->n = n;
+    this->n.normalize(); 
 }
 
-Plane::Plane(const Point3D &pt, const Vector3D &n)
-{
-    a(pt);
-    n(n);
-    n.normalize();
+Plane::Plane(const Plane &object) : a(object.a), n(object.n) {}
+
+Plane& Plane::operator=(const Plane &rhs) {
+    a = rhs.a;
+    n = rhs.n;
+    return *this;
 }
 
-Plane::Plane(const Plane &object)
-{
-    a(object.a);
-    n(object.n);
-}
-
-Plane& Plane::operator=(const Plane &rhs)
-{
-    a(rhs.a);
-    n(rhs.n);
-}
-
-virtual std::string Plane::to_string() const 
+std::string Plane::to_string() const 
 {
     return a.to_string() + " " + n.to_string();
 }
 
-virtual bool Plane::hit(const Ray &ray, float &t, ShadeInfo &s) const
+bool Plane::hit(const Ray &ray, float &t, ShadeInfo &s) const
 {
     float tmin = (a - ray.o) * n/(ray.d * n);
     if (tmin > kEpsilon)
     {
         t = tmin;
         s.normal = n;
-        s.hit_point = ray.o + t * ray.d;
+        s.normal.normalize();
+        s.hit_point = ray.o + (t * ray.d);
+        s.ray = ray; s.t = t;
         return true;
     }
     return false;
 }
 
-virtual BBox Plane::getBBox() const 
+BBox Plane::getBBox() const 
 {
-    return BBox();
+    BBox b;
+    return b;
 }
